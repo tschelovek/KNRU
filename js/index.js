5fb91f29262484f58780b4fd0963f46ae4c2457b
tschelovek <az18rg@gmail.com>
2024-06-29 21:05:13 +0300

    Page contacts ready

--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
     hideScrollbar: false,
   });
 
+  /**
+   * Слайдеры
+   */
+
   const swiper1 = new Swiper('#slider_projects', {
     loop: true,
     autoplay: {
@@ -55,6 +59,46 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   /**
+   * YANDEX.MAPS API
+   */
+
+  if(document.getElementById('map_spb')) {
+    ymaps.ready(function () {
+      let map = new ymaps.Map("map_spb", {
+        center: [59.941748, 30.349499],
+        zoom: 15
+      });
+
+      let myPlacemark = new ymaps.Placemark([59.941748, 30.349499], {
+        draggable: 1,
+        hintOptions: {
+          maxWidth: 100,
+          showTimeout: 200,
+          offset: [5,5]
+        },
+        balloonOptions: {
+          maxWidth: 70,
+          hasCloseButton: false,
+          mapAutoPan: 0
+        }
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: '../images/marker_YMaps.png',
+        iconImageSize: [54, 76],
+        iconImageOffset: [-27, -76]
+      });
+
+      myPlacemark.name = "KNRU";
+
+      map.behaviors.disable('scrollZoom');
+
+      map.geoObjects.add(myPlacemark);
+    });
+  }
+
+
+
+  /**
    * ~~МЕЛКИЕ СКРИПТЫ~~
    * =============
    */
@@ -74,4 +118,5 @@ document.addEventListener('DOMContentLoaded', () => {
    * ~~КОНЕЦ МЕЛКИХ СКРИПТОВ~~
    * =============
    */
+
 })
